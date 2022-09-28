766c5b9ee6ec71304c50613791211819eb3a883a
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-09-27 21:39:49 -0300

    Pull del branch develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/Home';
 
 function App() {
 	return (
-		<>
-			<h1>HOLA DESDE APP</h1>
-		</>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
 	);
-=======
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/Home";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
->>>>>>> 5d235ebb658a0cdcf80adae672bd9317455472ce
 }
 
 export default App;
