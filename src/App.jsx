cd51a55ec229405120e26f4f58ad1261fe8a8c22
sebaszwenger <sebaszwenger@gmail.com>
2022-09-27 21:30:09 -0300

    se agregaron las rutas a las paginas de Menu, Nosotros y Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Nosotros from "./pages/Nosotros";
+import Contacto from "./pages/Contacto";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </BrowserRouter>
   );
