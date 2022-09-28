52b5caf395d80c199ff924f88097c11771423a64
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-09-27 23:30:37 -0300

    se modificaron estilos

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contacto = () => {
   return (
-    <div className="w-full h-screen bg-yellow-100 flex justify-center items-center">
+    <div className="w-full h-screen bg-yellow-50 flex justify-center items-center">
       Contacto
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-yellow-100 flex justify-center items-center">
+    <div className="w-full h-screen bg-yellow-50 flex justify-center items-center">
       Home
     </div>
   );
