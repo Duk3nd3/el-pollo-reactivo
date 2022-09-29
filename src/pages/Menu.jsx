2a641357a787138e827afb9ad7050faa6b3c395b
Sebastián Zwenger <sebaszwenger@gmail.com>
2022-09-29 01:12:30 -0300

    se paso la tarjeta al componente MenuCard.

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,35 +1,11 @@
 import React from "react";
-
+import MenuCard from "../components/menu/MenuCard";
 const Menu = () => {
   return (
-    <div className="w-full h-screen bg-gray-200 flex justify-center items-center gap-3">
-      {/* menu card */}
-
-      <div className="w-[450px] h-[250px] bg-yellow-100 overflow-hidden border-zinc-700 shadow-md shadow-zinc-600 relative flex items-center">
-        <div className="w-[300px] h-[300px] order-double border-8 rounded-full border-white bg-[url('/img/canelones.jpg')] bg-cover g-center ounded-full hadow-lg shadow-gray-700 absolute -left-20 -top-8"></div>
-        <div className="absolute left-[55%] flex flex-col justify-center w-[45%] h-2/3 my-auto border-l-2 border-zinc-400 text-center">
-          <p className="font-bold text-2xl">Canelones</p>
-          <p className="">( Ingredientes )</p>
-        </div>
-      </div>
-
-      {/* menu card */}
-      <div className="w-[450px] h-[250px] bg-yellow-100 overflow-hidden border-zinc-700 shadow-md shadow-zinc-600 relative flex items-center">
-        <div className="w-[300px] h-[300px] order-double border-8 rounded-full border-white bg-[url('/img/canelones.jpg')] bg-cover g-center ounded-full hadow-lg shadow-gray-700 absolute -left-20 -top-8"></div>
-        <div className="absolute left-1/2 flex flex-col justify-center w-1/2 h-2/3 my-auto border-zinc-400 text-center">
-          <p className="font-bold text-2xl">Canelones</p>
-          <p className="">( Ingredientes )</p>
-        </div>
-      </div>
-
-      {/* menu card */}
-      <div className="w-[280px] h-[350px] bg-yellow-100 overflow-hidden border-zinc-700 shadow-md shadow-zinc-600 relative flex justify-center items-baseline">
-        <div className="w-[280px] h-[280px] border-8 border-white bg-[url('/img/canelones.jpg')] bg-cover bg-center"></div>
-        {/* <div className="absolute left-1/2 flex flex-col justify-center w-1/2 h-2/3 my-auto border-zinc-400 text-center">
-          <p className="font-bold text-2xl">Canelones</p>
-          <p className="">( Ingredientes )</p>
-        </div> */}
-      </div>
+    <div className="w-full h-screen bg-gray-200 pt-[200px]">
+      <ul className="max-w-[85%] h-screen mx-auto flex justify-center flex-wrap  gap-5 ">
+        <MenuCard />
+      </ul>
     </div>
   );
 };
