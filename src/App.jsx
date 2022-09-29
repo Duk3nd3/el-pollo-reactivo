3e37787571d9aebb80c330ec7c091059a6f0b478
sebaszwenger <sebaszwenger@gmail.com>
2022-09-29 13:58:07 -0300

    cambios de estilo. probando otro menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import Menu2 from "./pages/Menu2";
 import Nosotros from "./pages/Nosotros";
 import Contacto from "./pages/Contacto";
 
@@ -12,7 +13,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu" element={<Menu2 />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
