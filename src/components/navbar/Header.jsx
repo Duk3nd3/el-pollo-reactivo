0bb90bbfa1ab59cf13c731fcac43421287584fa2
sebaszwenger <sebaszwenger@gmail.com>
2022-10-24 11:32:10 -0300

    se cambiaron estilos. se agrego un header intermedio para pantallas medianas

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,18 +1,32 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Link to="/">
-      <header className="flex justify-center">
-        <img
-          src="/img/pollo-comilon-header.png"
-          alt="header El Pollo Comilon"
-          width={750}
-          className="hover:scale-105 ease-in duration-150 cursor-pointer pb-1"
-        />
+    <>
+      <header className="h-[120px] flex justify-center items-center">
+        <div className="hidden sm:block px-1">
+          <Link to="/">
+            <img
+              src="/img/pollo-comilon-header.png"
+              alt="El Pollo Comilon"
+              width={750}
+              className=""
+            />
+          </Link>
+        </div>
+        <div className="flex justify-center items-center gap-5 cursor-pointer px-2">
+          <div className="sm:hidden">
+            <img src="/img/polloADer.png" alt="logo El Pollo Comilon" />
+          </div>
+          <div className="sm:hidden ">
+            <img src="/img/el pollo comilon.png" alt="El Pollo Comilon" />
+          </div>
+          <div className="sm:hidden">
+            <img src="/img/polloAIzq.png" alt="logo El Pollo Comilon" />
+          </div>
+        </div>
       </header>
-    </Link>
+    </>
   );
 };
 
