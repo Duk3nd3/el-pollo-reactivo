f449a42d1f779a2c9e843689b8cb4065c606148f
sebaszwenger <sebaszwenger@gmail.com>
2022-11-03 13:42:47 -0300

    se creo una página provisoria para mostrar en pantalla los artículos del carrito de comparas

---
+++ src/pages/CarritoCompras.jsx
@@ -0,0 +1,50 @@
+import ProductCard from "../components/carritoCompras/ProductCard";
+import { useCart } from "../context/cartContext";
+
+const CarritoCompras = () => {
+  const { cart, clearCart } = useCart();
+  return (
+    <>
+      {cart.length === 0 || cart === undefined ? (
+        <div className="w-full h-screen flex flex-col justify-center items-center text-xl font-semibold">
+          <p className="">Aún no tienes productos en tu carrito</p>{" "}
+          <p className="">
+            Has click{" "}
+            <a className="text-red-600 font-black" href="/menu">
+              aqui
+            </a>{" "}
+            para agregar comidas{" "}
+          </p>
+        </div>
+      ) : (
+        <div className="pt-[240px] max-w-[95%] mx-auto xl:max-w-[80%]">
+          <h2 className="text-3xl text-red-600 font-semibold text-center">
+            Carrito De Compras
+          </h2>
+          <div className="w-full py-10 px-5 bg-zinc-200 mt-10">
+            <h3 className="text-start text-xl font-semibold text-orange-500 ">
+              Tus Productos
+            </h3>
+            <ul className="bg-orange-200 px-2 py-2">
+              {cart?.map((producto, index) => {
+                return (
+                  <li className="w-fit py-2" key={index}>
+                    <ProductCard producto={producto} />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <button
+            onClick={clearCart}
+            className="bg-red-600 p-1 text-white font-semibold rounded-md"
+          >
+            Eliminar carrito
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CarritoCompras;
