879df077b41ee6aee1fbb2e3c4d2b2df4587db31
sebaszwenger <sebaszwenger@gmail.com>
2022-11-03 13:26:34 -0300

    componente que permite seleccionar una cantidad de carne preestablecida

---
+++ src/components/menu/PesoTernera.jsx
@@ -0,0 +1,73 @@
+import { useMenu } from "../../context/menuContext";
+
+const PesoTernera = () => {
+  const { menu, setMenu } = useMenu();
+
+  const handleChange = (e) => {
+    setMenu({
+      ...menu,
+      cantidad: parseFloat(e.target.value),
+    });
+  };
+
+  return (
+    <form className="flex flex-wrap gap-2">
+      <fieldset className="border border-zinc-500 px-3 text-center">
+        <legend className="px-1 font-semibold">Cantidad (gramos):</legend>
+
+        <div className="py-1">
+          <input
+            onChange={handleChange}
+            type="radio"
+            id="gramos"
+            name="peso"
+            value={0.5}
+          />
+          <label htmlFor="gramos" className="ml-1 font-semibold">
+            500 <span className="text-red-500">grs</span>
+          </label>
+        </div>
+
+        <div className="py-1">
+          <input
+            onChange={handleChange}
+            type="radio"
+            id="gramos"
+            name="peso"
+            value={1}
+          />
+          <label htmlFor="gramos" className="ml-1 font-semibold">
+            1000 <span className="text-red-500">grs</span>
+          </label>
+        </div>
+
+        <div className="py-1">
+          <input
+            onChange={handleChange}
+            type="radio"
+            id="gramos"
+            name="peso"
+            value={1.5}
+          />
+          <label htmlFor="gramos" className="ml-1 font-semibold">
+            1500 <span className="text-red-500">grs</span>
+          </label>
+        </div>
+        <div className="py-1">
+          <input
+            onChange={handleChange}
+            type="radio"
+            id="gramos"
+            name="peso"
+            value={2}
+          />
+          <label htmlFor="gramos" className="ml-1 font-semibold">
+            2000 <span className="text-red-500">grs</span>
+          </label>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
+export default PesoTernera;
