bb39c0fadabb843de2513d93bae56dcde5e59dd1
sebaszwenger <sebaszwenger@gmail.com>
2022-11-03 13:34:07 -0300

    componente que muestra en pantalla los distintos tipos de guarniciones

---
+++ src/components/menu/menuPersonalizado/Guarniciones.jsx
@@ -0,0 +1,27 @@
+import { useMenu } from "../../../context/menuContext";
+import Plato from "../Plato";
+
+const Guarniciones = ({ guarnicion, handleGuarnicion }) => {
+  const { guarnicionSeleccionada } = useMenu();
+
+  return (
+    <li className="flex flex-col items-center">
+      <div>
+        <Plato
+          imagen={
+            <img
+              src={`/img/${guarnicion.nombre}.jpg`}
+              alt={guarnicion.monbre}
+            />
+          }
+          seleccion={guarnicionSeleccionada.nombre}
+          nombre={guarnicion.nombre}
+          onClick={() => handleGuarnicion(guarnicion)}
+        />
+      </div>
+      <h3 className="font-semibold">{guarnicion.nombre}</h3>
+    </li>
+  );
+};
+
+export default Guarniciones;
