4ba86e6a800ecaf9434f1bdfea832ac9474f2bd4
sebaszwenger <sebaszwenger@gmail.com>
2022-11-04 13:36:59 -0300

    Refactor: cree el componente "Menu" para mostrar todas las comidas que se pueden ordenar
    Al seleccionar una comida la pantalla se desplaza hacia dicho componente a fin de mejorar la experiencia de usuario.

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from "react";
+
+import TipoComidas from "../components/menu/TipoComidas";
+import MenuPersonalizado from "../components/menu/MenuPersonalizado";
+import Pollo from "../components/menu/Pollo";
+import Ternera from "../components/menu/Ternera";
+import { useMenu } from "../context/menuContext";
+import TipoGuarnicion from "../components/menu/TipoGuarnicion";
+
+const Menu = () => {
+  const {
+    comidas,
+    tipoDeComidaSeleccionada,
+    handleTipoDeComidas,
+    moveIntoView,
+    inicializarMenu,
+  } = useMenu();
+
+  useEffect(() => {
+    inicializarMenu();
+  }, []);
+
+  const tipoMenuRef = useRef();
+
+  const handleComidas = (comida) => {
+    handleTipoDeComidas(comida);
+    moveIntoView(tipoMenuRef);
+  };
+
+  return (
+    <div className="h-screen">
+      <h2 className="font-bold text-red-600 text-xl sm:text-2xl xl:text-4xl pt-[260px] b-10 text-center tracking-widest pb-20">
+        Que te gustaria comer hoy?
+      </h2>
+      <ul className="max-w-[95%] xl:max-w-[80%] mx-auto flex justify-center flex-wrap gap-2 sm:gap-3 lg:gap-4 xl:gap-5 pb-10">
+        {comidas.map((comida) => (
+          <TipoComidas
+            handleComidas={handleComidas}
+            comida={comida}
+            key={comida.id}
+          />
+        ))}
+      </ul>
+
+      <div id="tipoComida" ref={tipoMenuRef} className="pt-[220px]">
+        {tipoDeComidaSeleccionada === "menu personalizado" && (
+          <MenuPersonalizado />
+        )}
+
+        {tipoDeComidaSeleccionada === "carne a la parrilla" && <Ternera />}
+        {tipoDeComidaSeleccionada === "pollo a la parrilla" && <Pollo />}
+        {tipoDeComidaSeleccionada === "guarniciones" && <TipoGuarnicion />}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
