c2d0b50c019478eda92132fbc282e1717a6b745b
sebaszwenger <sebaszwenger@gmail.com>
2022-11-05 10:47:09 -0300

    refactor: Añadi el state "contador" e invoque el state global "menu"
    Añadi el state "contador" para pasarselo al componente "Cantidad". Añadi un useEffect para actualizar la propiedad "cantidad" del state global "menu" cada vez que se modifique el state "contador".

--- src/components/menu/TipoGuarnicion.jsx
+++ src/components/menu/TipoGuarnicion.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useMenu } from "../../context/menuContext";
 import Cantidad from "./Cantidad";
 import ListaGuarniciones from "./menuPersonalizado/ListaGuarniciones";
 import SubmitCarritoButton from "./SubmitCarritoButton";
 
 const Guarniciones = () => {
-  const { menu, handleSubmit } = useMenu();
+  const { menu, setMenu, handleSubmit } = useMenu();
+
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    setMenu({ ...menu, cantidad: contador });
+  }, [contador]);
 
   return (
     <div className="h-screen max-w-[95%] xl:max-w-[80%] mx-auto flex flex-col items-center gap-5">
@@ -18,7 +24,7 @@ const Guarniciones = () => {
             </p>
           )}
 
-          <Cantidad />
+          <Cantidad contador={contador} setContador={setContador} />
         </div>
       ) : (
         <p className="text-red-600 animate-pulse">Selecciona una guarnición</p>
