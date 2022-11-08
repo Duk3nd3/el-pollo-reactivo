ed5be4be04b4431a95876184b5e17fe48653be09
sebaszwenger <sebaszwenger@gmail.com>
2022-11-08 11:22:33 -0300

    style: Cambie los datos que se muestran en pantalla
    cambie la forma en la que se muestra la guarnicion del menu para cuando el tipo de comida seleccionada sea "empanadas".

--- src/components/carritoCompras/ProductCard.jsx
+++ src/components/carritoCompras/ProductCard.jsx
@@ -1,15 +1,29 @@
 import { useCart } from "../../context/cartContext";
-
 const ProductCard = ({ producto }) => {
   const { removeFromCart } = useCart();
-
   return (
     <div className="bg-red-200 p-2">
-      <p>{producto.ingredientePrincipal}</p>
-      {producto.guarnicion && <p>{producto.guarnicion}</p>}
+      <p className="capitalize">{producto?.ingredientePrincipal}</p>
 
-      <p>Precio: {producto.precio * producto.cantidad}</p>
-      <p>Cantidad: {producto.cantidad}</p>
+      {producto.ingredientePrincipal === "empanadas"
+        ? Object.keys(producto?.guarnicion).map((key, i) => {
+            if (producto.guarnicion[key] !== 0) {
+              return (
+                <p key={i}>
+                  <span className="capitalize">
+                    {key === "jamon" ? "jamon y queso" : key}:{" "}
+                  </span>
+                  <span>{producto.guarnicion[key]}</span>
+                </p>
+              );
+            }
+          })
+        : producto.guarnicion && <p>{producto.guarnicion}</p>}
+      {producto.ingredientePrincipal !== "empanadas" && (
+        <p>Cantidad: {producto.cantidad}</p>
+      )}
+      <p>Precio: {producto.precio * producto.cantidad}$</p>
+
       <button
         onClick={() => removeFromCart(producto.id)}
         className="bg-red-600 text-white font-semibold p-1 rounded-md"
