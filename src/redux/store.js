c465c9e29c404a5168c1f962937b56feec5553b9
sebaszwenger <sebaszwenger@gmail.com>
2022-11-10 10:58:47 -0300

    refactor: agregue el cartReducers al store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducers from "../features/cart/cartSlice";
 
-export const store = configureStore({});
+export const store = configureStore({
+  cart: cartReducers,
+});
