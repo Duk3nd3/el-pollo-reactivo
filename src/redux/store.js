e39b927c7c7d166ec34c07b36dc63d1d2a584848
sebaszwenger <sebaszwenger@gmail.com>
2022-11-16 21:15:34 -0300

    refactor: agregue el reducer de menu

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import menuReducer from "../features/menu/menuSlice";
 
 export const store = configureStore({
-  reducer: { cart: cartReducer },
+  reducer: { cart: cartReducer, menu: menuReducer },
 });
