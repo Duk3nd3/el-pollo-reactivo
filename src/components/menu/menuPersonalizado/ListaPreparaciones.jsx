d96b05d1ea7e75833f64203eb61dc8fe9aa44d55
sebaszwenger <sebaszwenger@gmail.com>
2022-11-17 20:50:42 -0300

    refactor: Realice cambios en funciones y estados.
    Importe useSelector.
    Importe el menu desde el store.
    Reemplace el state tipoMenuSeleccionado por menu.ingredienteSeleccionado.
    Cambie nombre func handlePreparacion por handlePreparacionSeleccionada

--- src/components/menu/menuPersonalizado/ListaPreparaciones.jsx
+++ src/components/menu/menuPersonalizado/ListaPreparaciones.jsx
@@ -1,24 +1,26 @@
-import React from "react";
 import { useMenu } from "../../../context/menuContext";
+import { useSelector } from "react-redux";
 import PreparacionesCard from "./PreparacionesCard";
 
-const ListaPreparaciones = ({ handlePreparacion }) => {
-  const { preparaciones, tipoMenuSeleccionado } = useMenu();
+const ListaPreparaciones = ({ handlePreparacionSeleccionada }) => {
+  const menu = useSelector((state) => state.menu);
+  const { preparaciones } = useMenu();
+
   return (
     <>
       <h3 className="font-bold text-red-600 text-xl sm:text-2xl xl:text-4xl pb-20 xl:pt-5 tracking-widest text-center">
         {`Como quieres que preparemos tu `}
         <span className="font-bold text-red-600 capitalize">
-          {tipoMenuSeleccionado}
+          {menu.ingredienteSeleccionado}
         </span>
       </h3>
       <ul className="flex justify-center flex-wrap gap-2 sm:gap-3 lg:gap-4 xl:gap-5 pb-10">
         {preparaciones
-          ?.filter((item) => item.categoria == tipoMenuSeleccionado)
+          ?.filter((item) => item.categoria == menu.ingredienteSeleccionado)
           ?.map((preparacion) => (
             <PreparacionesCard
               preparacion={preparacion}
-              handlePreparacion={handlePreparacion}
+              handlePreparacionSeleccionada={handlePreparacionSeleccionada}
               key={preparacion.id}
             />
           ))}
