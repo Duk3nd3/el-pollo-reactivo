d7cdc6093669be362cc6dcbf38fa1d0a30e85ca8
sebaszwenger <sebaszwenger@gmail.com>
2022-11-17 19:43:32 -0300

    refactor: Elimine el componente SubmitCarritoButton
    Elimine la importacion del contexto MenuContext, no se necesitaba mas.

--- src/components/menu/ternera/Ternera.jsx
+++ src/components/menu/ternera/Ternera.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import { useMenu } from "../../../context/menuContext";
 import Plato from "../Plato";
 import PesoTernera from "./PesoTernera";
-import SubmitCarritoButton from "../SubmitCarritoButton";
 
 const Ternera = () => {
-  const { menu, setMenu, handleSubmit } = useMenu();
-
   return (
-    <div className="h-screen max-w-[90%] xl:max-w-[80%] m-auto text-center">
+    <div className="max-w-[90%] xl:max-w-[80%] m-auto text-center">
       <h3 className="font-bold text-red-600 text-xl sm:text-2xl xl:text-4xl pb-10 tracking-widest text-center">
         Carne a la parrilla x Kg
       </h3>
@@ -24,13 +19,8 @@ const Ternera = () => {
             nombre={"carne a la parrilla"}
           />
         </div>
-        <PesoTernera setMenu={setMenu} menu={menu} />
+        <PesoTernera />
       </div>
-      {menu.cantidad > 0 ? (
-        <SubmitCarritoButton handleSubmit={handleSubmit} disabled={false} />
-      ) : (
-        <SubmitCarritoButton handleSubmit={handleSubmit} disabled={true} />
-      )}
     </div>
   );
 };
