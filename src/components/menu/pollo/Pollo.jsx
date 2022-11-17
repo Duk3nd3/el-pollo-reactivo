f940cc307e6084f7444e3955c26fd409f9d3adad
sebaszwenger <sebaszwenger@gmail.com>
2022-11-17 20:01:40 -0300

    refactor: Elimine el uso del state menu anterior
    Elimine el componente SubmitCarritoButton.
    Modifique estilos.

--- src/components/menu/pollo/Pollo.jsx
+++ src/components/menu/pollo/Pollo.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import { useMenu } from "../../../context/menuContext";
 import Plato from "../Plato";
 import PorcionPollo from "./PorcionPollo";
-import SubmitCarritoButton from "../SubmitCarritoButton";
 
 const Pollo = () => {
-  const { menu, handleSubmit } = useMenu();
-
   return (
-    <div className="h-screen max-w-[90%] xl:max-w-[80%] m-auto text-center flex flex-col items-center">
+    <div className="max-w-[90%] xl:max-w-[80%] m-auto text-center flex flex-col items-center">
       <h3 className="font-bold text-red-600 text-xl sm:text-2xl xl:text-4xl pb-10 tracking-widest text-center">
         Pollo a la parrilla
       </h3>
@@ -24,12 +19,6 @@ const Pollo = () => {
         />
         <PorcionPollo />
       </div>
-
-      {menu.cantidad > 0 ? (
-        <SubmitCarritoButton handleSubmit={handleSubmit} disabled={false} />
-      ) : (
-        <SubmitCarritoButton handleSubmit={handleSubmit} disabled={true} />
-      )}
     </div>
   );
 };
