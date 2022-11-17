be6560aa07baa2e69c63d8d5d9c6e2a63b7df245
sebaszwenger <sebaszwenger@gmail.com>
2022-11-17 20:10:34 -0300

    refactor: Elimine el uso del state menu anterior
    Importe useDispatch y useSelector.
    Importe e implemente la funcion addCantidad del menuSlice.
    Add handleClick para actualizar el contador del componente Cantidad.
    reemplace guarnicionSeleccionada por menu.guarnicion.
    Reemplace preparacionSeleccionada por menu.ingredientePrincipal

--- src/components/menu/menuPersonalizado/MenuPersonalizadoCard.jsx
+++ src/components/menu/menuPersonalizado/MenuPersonalizadoCard.jsx
@@ -1,29 +1,38 @@
 import { useEffect, useState } from "react";
-import { useMenu } from "../../../context/menuContext";
+import { useDispatch, useSelector } from "react-redux";
+import { addCantidad } from "../../../features/menu/menuSlice";
 import Cantidad from "../Cantidad";
 import Plato from "../Plato";
 
 const MenuPersonalizadoCard = () => {
-  const { menu, setMenu } = useMenu();
+  const menu = useSelector((state) => state.menu);
+  const dispatch = useDispatch();
 
-  const { guarnicionSeleccionada, preparacionSeleccionada } = useMenu();
   const [contador, setContador] = useState(0);
 
   useEffect(() => {
-    setMenu({ ...menu, cantidad: contador });
+    dispatch(addCantidad(contador));
   }, [contador]);
+
+  const handleClick = (e) => {
+    if (e.target.innerHTML === "+") {
+      setContador(contador + 1);
+    } else if (contador > 0) {
+      setContador(contador - 1);
+    }
+  };
 
   return (
     <div className="mb-5">
       <h3 className="font-semibold text-zinc-800 text-center text-xl sm:text-2xl xl:text-3xl py-6 tracking-widest pb-20">
         El menú seleccionado es:{" "}
         <span className="font-bold text-red-600 capitalize">
-          {preparacionSeleccionada.nombre}
+          {menu.ingredientePrincipal}
         </span>
-        {guarnicionSeleccionada !== "" && (
+        {menu.guarnicion !== "" && (
           <span className="font-bold text-red-600 capitalize">
             {" "}
-            con {guarnicionSeleccionada.nombre}
+            con {menu.guarnicion}
           </span>
         )}
       </h3>
@@ -33,34 +42,30 @@ const MenuPersonalizadoCard = () => {
           <Plato
             imagen={
               <img
-                src={`/img/${preparacionSeleccionada.nombre}.jpg`}
-                alt={preparacionSeleccionada.monbre}
+                src={`/img/${menu.ingredientePrincipal}.jpg`}
+                alt={menu.ingredientePrincipal}
               />
             }
           />
-          <h3 className="font-semibold pt-2">
-            {preparacionSeleccionada.nombre}
-          </h3>
+          <h3 className="font-semibold pt-2">{menu.ingredientePrincipal}</h3>
         </div>
 
-        {guarnicionSeleccionada !== "" && (
+        {menu.guarnicion !== "" && (
           <div className="text-center">
             <Plato
               imagen={
                 <img
-                  src={`/img/${guarnicionSeleccionada.nombre}.jpg`}
-                  alt={guarnicionSeleccionada.monbre}
+                  src={`/img/${menu.guarnicion}.jpg`}
+                  alt={menu.guarnicion.monbre}
                 />
               }
             />
-            <h3 className="font-semibold pt-2">
-              {guarnicionSeleccionada.nombre}
-            </h3>
+            <h3 className="font-semibold pt-2">{menu.guarnicion}</h3>
           </div>
         )}
       </div>
       <p className="text-red-600 font-semibold">Selecciona una cantidad</p>
-      <Cantidad contador={contador} setContador={setContador} />
+      <Cantidad handleClick={handleClick} contador={contador} />
     </div>
   );
 };
