23ee167b06d33674bce7b6e11014678add7057fd
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-12-01 23:05:30 -0300

    Fix sobre el componente Cantidad.jsx - Error al disminuir cantidad de porciones

--- src/components/menu/Cantidad.jsx
+++ src/components/menu/Cantidad.jsx
@@ -9,7 +9,7 @@ const Cantidad = ({ contador, setContador }) => {
 
 	return (
 		<>
-			<div className='bg-zinc-700 order border-zinc-500 shadow-md shadow-zinc-700 my-2 w-fit mx-auto text-xl'>
+			<div className='bg-zinc-700 order border-zinc-500 shadow-md shadow-zinc-700 my-2 w-fit mx-auto text-l'>
 				<button
 					onClick={handleClick}
 					className='bg-orange-300 font-bold w-8 py-1'
