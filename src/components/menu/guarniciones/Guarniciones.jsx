cc961e3846bbd2cf10ee05fff10f7c1271aa0f7c
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-12-01 23:32:57 -0300

    Fix de estilos/animaciones sobre componentes referentes a Menu

--- src/components/menu/guarniciones/Guarniciones.jsx
+++ src/components/menu/guarniciones/Guarniciones.jsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState, useRef } from "react";
-import { useMenu } from "../../../context/menuContext";
-import Cantidad from "../Cantidad";
-import ListaGuarniciones from "../ListaGuarniciones";
-import SubmitCarritoButton from "../SubmitCarritoButton";
+import React, { useEffect, useState, useRef } from 'react';
+import { useMenu } from '../../../context/menuContext';
+import Cantidad from '../Cantidad';
+import ListaGuarniciones from '../ListaGuarniciones';
+import SubmitCarritoButton from '../SubmitCarritoButton';
 
 const Guarniciones = () => {
-  const cantidadRef = useRef();
+	const cantidadRef = useRef();
 
-  const {
-    menu,
-    setMenu,
-    handleSubmit,
-    moveIntoView,
-    handleGuarnicionSeleccionada,
-  } = useMenu();
+	const {
+		menu,
+		setMenu,
+		handleSubmit,
+		moveIntoView,
+		handleGuarnicionSeleccionada,
+	} = useMenu();
 
-  const [contador, setContador] = useState(0);
+	const [contador, setContador] = useState(0);
 
-  useEffect(() => {
-    setMenu({ ...menu, cantidad: contador });
-  }, [contador]);
+	useEffect(() => {
+		setMenu({ ...menu, cantidad: contador });
+	}, [contador]);
 
-  const handleGuarnicion = (guarnicion) => {
-    handleGuarnicionSeleccionada(guarnicion);
-    moveIntoView(cantidadRef);
-  };
+	const handleGuarnicion = (guarnicion) => {
+		handleGuarnicionSeleccionada(guarnicion);
+		moveIntoView(cantidadRef);
+	};
 
-  return (
-    <div className="h-screen max-w-[95%] xl:max-w-[80%] mx-auto flex flex-col items-center gap-5">
-      <ListaGuarniciones handleGuarnicion={handleGuarnicion} />
-      {menu.guarnicion ? (
-        <div id="cantidad" ref={cantidadRef}>
-          {menu.cantidad < 1 && (
-            <p className="text-red-600 animate-pulse">
-              Selecciona una cantidad
-            </p>
-          )}
+	return (
+		<div className='h-screen max-w-[95%] xl:max-w-[80%] mx-auto flex flex-col items-center gap-5'>
+			<ListaGuarniciones handleGuarnicion={handleGuarnicion} />
+			{menu.guarnicion ? (
+				<div id='cantidad' ref={cantidadRef}>
+					{menu.cantidad < 1 && (
+						<p className='text-red-600 animate-pulse transition duration-500 ease-in-out hover: transform hover:-translate-y-1'>
+							Selecciona una cantidad
+						</p>
+					)}
 
-          <Cantidad contador={contador} setContador={setContador} />
-        </div>
-      ) : (
-        <p className="text-red-600 animate-pulse">Selecciona una guarnición</p>
-      )}
+					<Cantidad contador={contador} setContador={setContador} />
+				</div>
+			) : (
+				<p className='text-red-600 animate-pulse'>Selecciona una guarnición</p>
+			)}
 
-      <SubmitCarritoButton
-        handleSubmit={handleSubmit}
-        disabled={menu.cantidad > 0 ? false : true}
-      />
-    </div>
-  );
+			<SubmitCarritoButton
+				handleSubmit={handleSubmit}
+				disabled={menu.cantidad > 0 ? false : true}
+			/>
+		</div>
+	);
 };
 
 export default Guarniciones;
