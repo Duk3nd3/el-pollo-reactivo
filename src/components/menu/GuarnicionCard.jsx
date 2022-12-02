b4b20be01eb3311ed4d6b19b2fed1ec9c6f8c9d5
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-12-01 23:16:40 -0300

    Fix sobre componente GuarnicionCard.jsx - Error en el nombre de la seleccion del producto

--- src/components/menu/GuarnicionCard.jsx
+++ src/components/menu/GuarnicionCard.jsx
@@ -1,27 +1,27 @@
-import { useMenu } from "../../context/menuContext";
-import Plato from "./Plato";
+import { useMenu } from '../../context/menuContext';
+import Plato from './Plato';
 
 const GuarnicionCard = ({ guarnicion, handleGuarnicion }) => {
-  const { guarnicionSeleccionada } = useMenu();
+	const { guarnicionSeleccionada } = useMenu();
 
-  return (
-    <li className="flex flex-col items-center">
-      <div>
-        <Plato
-          imagen={
-            <img
-              src={`/img/${guarnicion.nombre}.jpg`}
-              alt={guarnicion.monbre}
-            />
-          }
-          seleccion={guarnicionSeleccionada.nombre}
-          nombre={guarnicion.nombre}
-          onClick={() => handleGuarnicion(guarnicion)}
-        />
-      </div>
-      <h3 className="font-semibold">{guarnicion.nombre}</h3>
-    </li>
-  );
+	return (
+		<li className='flex flex-col items-center'>
+			<div>
+				<Plato
+					imagen={
+						<img
+							src={`/img/${guarnicion.nombre}.jpg`}
+							alt={guarnicion.nombre}
+						/>
+					}
+					seleccion={guarnicionSeleccionada.nombre}
+					nombre={guarnicion.nombre}
+					onClick={() => handleGuarnicion(guarnicion)}
+				/>
+			</div>
+			<h3 className='font-semibold'>{guarnicion.nombre}</h3>
+		</li>
+	);
 };
 
 export default GuarnicionCard;
